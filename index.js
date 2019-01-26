2a1b7952cc825ec6af2ba9f53c25db32b3073fd7
Robert Osonma <35873421+master248@users.noreply.github.com>
2019-01-26 17:39:27 -0600

    changing to get detailed error messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var playerCount;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,12 +9,13 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  playerCount = playerCount + 1;
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('chat message', 'server says ' + msg);
   });
 });
 
