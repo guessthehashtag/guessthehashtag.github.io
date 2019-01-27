d7e2993919f9dc72719dc70c0b57597d9fe7c5fe
Robert Osonma <35873421+master248@users.noreply.github.com>
2019-01-27 01:58:57 -0600

    working on player object

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var playerCount = 0;
 var userNames = [];
+var user = new Object();
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
